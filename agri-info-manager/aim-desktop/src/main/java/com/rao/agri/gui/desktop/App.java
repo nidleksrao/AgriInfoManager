4155d1decdb9866b64d3bf539d2a2809b5d5db6b
unknown <176399@PC264195.cts.com>
2014-02-25 12:41:38 +0530

    Initial code set up

---
+++ agri-info-manager/aim-desktop/src/main/java/com/rao/agri/gui/desktop/App.java
@@ -0,0 +1,13 @@
+package com.rao.agri.gui.desktop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
